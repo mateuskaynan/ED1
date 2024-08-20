45dd4a0ad44a2601480997126f07e5a915331c92
Mateus Kaynan <mateuskaynan41@gmail.com>
2024-08-20 11:35:14 -0300

    add

--- SEMINÁRIO - Gnomo Sort/MELHOR CASO/num_melhor_caso_main.c
+++ SEMINÁRIO - Gnomo Sort/MELHOR CASO/num_melhor_caso_main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 #define TAMANHO_MAXIMO 9
 #include "numeros_melhor_caso.h"
 
 int main(){
+    
+    clock_t start, end; 
+    double time_total;
+    start = clock();
+    
     int dados[TAMANHO_MAXIMO] = {2, 3, 4, 1, 5, 7, 6, 8, 9}; /*VETOR E SEUS NÚMEROS JÁ PRÉ DEFINIDOS*/
     int tamanho = TAMANHO_MAXIMO; /*VARÁVEL RESPONSÁVEL POR ARMAZENAR A QUANTIDADE DE DADOS DENTRO DO VETOR/ARRAY*/  
 
@@ -19,5 +25,10 @@ int main(){
     printf("\nDADOS DO VETOR DEPOIS DA ORDENACAO: ");
     mostrarDados(dados, tamanho); 
     printf("----------------------------------------------------");
+    
+    end = clock();
+    time_total = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nTEMPO DE EXECUCAO: %f segundos\n", time_total);
+    
     return 0; 
 }
